193f092421244e1e36237d4c39f9d8c11794de52
DanilaTing <tinggone@gmail.com>
2020-05-20 05:55:37 +0300

    speed change

--- src/App.js
+++ src/App.js
@@ -168,13 +168,13 @@ export default class App extends Component {
     });
 
     if (this.r <= biggestSide * 2.2 && this.scale == "up") {
-      this.r += offset.y / 25;
+      this.r += offset.y / 15;
       // this.y -= offset.y / 9;
     } else if (this.r >= biggestSide * 2.2 && this.scale == "up") {
       this.scale = "down";
     } else if (this.r >= 0 && this.y <= height * 2.3 && this.scale == "down") {
-      this.r -= offset.y / 100;
-      this.y += offset.y / 40;
+      this.r -= offset.y / 50;
+      this.y += offset.y / 20;
     } else if (this.y >= height * 2.3 && this.scale == "down") {
       this.r = 0;
       this.y = window.innerHeight;
